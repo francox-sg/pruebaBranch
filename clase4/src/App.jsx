1e26c5138df56718df4d9203933add82b49f9ae9
Franco Galeano <francoxsg@gmail.com>
2024-03-05 00:22:36 -0300

    ejercicios

--- clase4/src/App.jsx
+++ clase4/src/App.jsx
@@ -1,24 +1,14 @@
-import Contador from './components/contador/contador'
-import Componente from './components/Componente/Componente';
-import { useState, useEffect } from 'react';
+
 import './App.css'
 
 function App() {
   
   const [show, setShow] = useState(false);
 
-/*   useEffect( ()=>{
-    setShow(show => show ? false : true)
-  }, [] ) */
-
-  const toggle = ()=>{
-    setShow(show => show ? false : true)
-  }
-
+  
   return (
     <>
-      <button onClick={toggle}>Mostrar</button>
-      {show ? <Componente/>: null}
+      
     </>
 
     )
